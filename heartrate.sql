08435f47e88093a7b80edae6cd377ea2e3ab9717
shafiq <shafiq_razman@yahoo.com>
2021-07-14 12:31:49 +0800

    adding files

---
+++ heartrate.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 13, 2021 at 08:42 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id17188317_heart_rate`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `heartrate`
+--
+
+CREATE TABLE `heartrate` (
+  `id` int(100) NOT NULL,
+  `heartrate` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `heartrate`
+--
+
+INSERT INTO `heartrate` (`id`, `heartrate`) VALUES
+(1, 100),
+(2, 100),
+(3, 100);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `heartrate`
+--
+ALTER TABLE `heartrate`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `heartrate`
+--
+ALTER TABLE `heartrate`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
